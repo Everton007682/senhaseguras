b7b7551171e41b53a806ebe37ddcc85e345cb046
Everton007682 <everton.serafim@escola.pr.gov.br>
2026-07-28 17:30:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
+let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
@@ -11,29 +12,51 @@ function diminuiTamanho(){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
-    geraSenha ();
+    geraSenha();
 }
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
-    geraSenha ();
+    geraSenha();
 }
-const campoSenha = document. querySelector('#campo-senha');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll ('.checkbox');
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-geraSenha ();
+for (i=0; i < checkbox.length;i++){
+    checkbox[i].onclick = geraSenha
+}
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
 
-campoSenha.value = letrasMaiusculas;
-geraSenha ();
+console.log(checkbox[1].checked);
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMaiusculas = 'abcdefghijklmnopqrstuvwxyz'
+const numeros = '0123456789';
+const simbolos = '!@%*/?';
 
 function geraSenha(){
     let senha = '';
-    for (let i=0 i tamanhoSenha;i++);{
-         let numeroAleatorio = Math.random()*letrasMaiusculas.legth;
+let alfabeto = '';
+if (chrckbox[0].checked){
+     alfabeto = alfabeto + letrasMaiusculas;
+        }
+if (chrckbox[1].checked){
+     alfabeto = alfabeto + letrasMaiusculas;
+        }
+if (chrckbox[2].checked){
+     alfabeto = alfabeto + numeros;
+        }
+if (chrckbox[3].checked){
+     alfabeto = alfabeto + simbolos;
+        }
+  
+    for (let i = 0; i < tamanhoSenha;i++) {
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
     numeroAleatorio = Math.floor(numeroAleatorio);
-    senha = senha + letrasMaiusculas[numeroAleatorio]
+    senha = senha + letrasMaiusculas[numeroAleatorio];
     }
-
+    campoSenha.value = senha;
 }
